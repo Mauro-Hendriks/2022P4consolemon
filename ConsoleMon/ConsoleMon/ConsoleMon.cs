5163be48d364c916d2e623120cebc31e45cdb7d1
Bor Dekker <32562@ma-web.nl>
2022-05-09 14:44:50 +0200

    Opdracht Classes Deel 1

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    class ConsoleMon
+    {
+        string name = "Pickachu"; 
+        int health = 10;
+        int energy = 200;
+
+    }
+}
