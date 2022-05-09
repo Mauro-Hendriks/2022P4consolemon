76b0bcbad9364f2f678f7cf6bb1b69d9062d3760
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2022-05-09 14:49:52 +0200

    added Opdracht Classes Deel 2

---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+
+    class Skill
+    {
+        string name = "thunder bolt";
+        int damage = 2;
+        int EnergyCost = 30;
+    }
+}
